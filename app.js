3093d4db67d33bfa4f210df60efef7102829f9ee
Karina <karinabarquero5@gmail.com>
2022-02-21 21:11:34 -0600

    Creando el archivo index.html y el archivo app.js con la funcionalidad de cambiar el color de fondo al presionar cualquier tecla del abecedario

---
+++ app.js
@@ -0,0 +1,58 @@
+document.body.style.backgroundColor = "White";
+document.addEventListener("keypress", (e) => {
+  if (e.key === "q") {
+    document.body.style.backgroundColor = "#fbf8cc";
+  } else if (e.key === "w") {
+    document.body.style.backgroundColor = "#0096c7";
+  } else if (e.key === "e") {
+    document.body.style.backgroundColor = "#8eecf5";
+  } else if (e.key === "r") {
+    document.body.style.backgroundColor = "#fec5bb";
+  } else if (e.key === "t") {
+    document.body.style.backgroundColor = "#cdb4db";
+  } else if (e.key === "y") {
+    document.body.style.backgroundColor = "#cfe1b9";
+  } else if (e.key === "u") {
+    document.body.style.backgroundColor = "#f28482";
+  } else if (e.key === "i") {
+    document.body.style.backgroundColor = "#f5cac3";
+  } else if (e.key === "o") {
+    document.body.style.backgroundColor = "#f6bd60";
+  } else if (e.key === "p") {
+    document.body.style.backgroundColor = "#a2d2ff";
+  } else if (e.key === "a") {
+    document.body.style.backgroundColor = "#ff758f";
+  } else if (e.key === "s") {
+    document.body.style.backgroundColor = "#ffccd5";
+  } else if (e.key === "d") {
+    document.body.style.backgroundColor = "#ddbdfc";
+  } else if (e.key === "f") {
+    document.body.style.backgroundColor = "#bbadff";
+  } else if (e.key === "g") {
+    document.body.style.backgroundColor = "#8e94f2";
+  } else if (e.key === "h") {
+    document.body.style.backgroundColor = "#fde2e4";
+  } else if (e.key === "j") {
+    document.body.style.backgroundColor = "#cddafd";
+  } else if (e.key === "k") {
+    document.body.style.backgroundColor = "#76c893";
+  } else if (e.key === "l") {
+    document.body.style.backgroundColor = "#34a0a4";
+  } else if (e.key === "ñ") {
+    document.body.style.backgroundColor = "#b5e48c";
+  } else if (e.key === "z") {
+    document.body.style.backgroundColor = "#ffb4a2";
+  } else if (e.key === "x") {
+    document.body.style.backgroundColor = "#64dfdf";
+  } else if (e.key === "c") {
+    document.body.style.backgroundColor = "#ffa6c1";
+  } else if (e.key === "v") {
+    document.body.style.backgroundColor = "#7d82b8";
+  } else if (e.key === "b") {
+    document.body.style.backgroundColor = "#d8bbff";
+  } else if (e.key === "n") {
+    document.body.style.backgroundColor = "#b2f7ef";
+  } else if (e.key === "m") {
+    document.body.style.backgroundColor = "#d55d92";
+  }
+});
